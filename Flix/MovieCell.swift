dc3fc6082b6145e8fcc046d5f6d2f3503ebb6fed
Pavani Malli <pavanim@fb.com>
2017-06-21 16:43:42 -0700

    added Alamofire

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Pavani Malli on 6/21/17.
+//  Copyright © 2017 Pavani Malli. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
