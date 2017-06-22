f2d112815a30ce2ccb09af3228050e3ea3e078ff
Pavani Malli <pavanim@fb.com>
2017-06-22 16:06:53 -0700

    Incorporated UI elements into detail class

--- Flix/MovieDetailViewController.swift
+++ Flix/MovieDetailViewController.swift
@@ -7,29 +7,57 @@
 //
 
 import UIKit
+import AlamofireImage
+
+
+enum MovieKeys {
+    static let title = "title"
+    static let overview = "overview"
+    static let backdrop = "backdrop_path"
+    static let poster = "poster_path"
+    static let releaseDate = "release_date"
+}
 
 class MovieDetailViewController: UIViewController {
 
+    
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var backdropImageView: UIImageView!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    var movie: [String:Any]?
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let movie = movie {
+            titleLabel.text = movie[MovieKeys.title] as? String
+            overviewLabel.text = movie[MovieKeys.overview] as? String
+            releaseDateLabel.text = movie[MovieKeys.releaseDate] as? String
+            let backDropPath = movie[MovieKeys.backdrop] as! String
+            let posterPath = movie[MovieKeys.poster] as! String
+            
+            let baseURLString = "https://image.tmdb.org/t/p/w500"
+            
+            let posterURL = URL(string: baseURLString + posterPath)!
+            let backdropURL = URL(string:baseURLString + backDropPath)!
+            
+            backdropImageView.af_setImage(withURL: backdropURL)
+            posterImageView.af_setImage(withURL: posterURL)
+        }
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
